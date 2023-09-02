f3531da39ba709642d4b57b02c4beb6c693b41a0
Nadeem <108587101+ndmxx@users.noreply.github.com>
2023-09-02 18:59:45 +0530

    1st commit variables, input output Functions notes

---
+++ Input and Output Functions.py
@@ -0,0 +1,21 @@
+#INPUT AND OUTPUT FUNCTIONS
+
+#OUTPUT FUNCTIONS
+
+    #Fuctin used to display or print a message in console
+        #printf(C)
+        #cout(C++)
+        #print(python)
+
+
+#EXAMPLE
+
+print("Hello Nadeem")
+print("I'm from Cochin")
+print('My age is 21')
+
+
+
+company_name = "Brototype"
+location = "Maradu"
+print(company_name)
