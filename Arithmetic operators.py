4232449399653f45149489a1ab4b97d8b9ce5487
Muhammed Nadeem <nadeemvezhappilly@gmail.com>
2023-09-03 12:27:15 +0530

    1st commit variables, input output Functions notes

---
+++ Arithmetic operators.py
@@ -0,0 +1,17 @@
+#ARITHMETIC OPERATORS
+
+    #1 ADDITION(+)
+
+    #2 SUBTRACTION(-)
+
+    #3 MULTIPLICATION(*)
+
+    #4 DIVISION(/)
+
+    #5 MODULUS(%)
+
+    #6 POWER(**)
+
+    #7 FLOOR DIVISION(//)       //To discard decimal point
+
+    
---
+++ Exercise No.1(input output).py
@@ -0,0 +1,10 @@
+#SWAP 2 NUMBERS
+
+num1 = input("Enter the first number")
+num2 = input("Enter the second number")
+print("First number is ",num1,"\nSecond number is ",num2)
+temp = num1
+num1 = num2
+num2 = temp
+print("Numbers after swapping:-")
+print("First number = ",num1 ,"\nSecond number = ",num2)
