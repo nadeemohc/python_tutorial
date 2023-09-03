85d4af34df33591591e5d204ff31d4e88f059930
Muhammed Nadeem <nadeemvezhappilly@gmail.com>
2023-09-03 10:08:14 +0530

    1st commit variables, input output Functions notes

--- Input and Output Functions.py
+++ Input and Output Functions.py
@@ -2,7 +2,7 @@
 
 #OUTPUT FUNCTIONS
 
-    #Fuctin used to display or print a message in console
+    #Fuction used to display or print a message in console
         #printf(C)
         #cout(C++)
         #print(python)
@@ -10,18 +10,21 @@
 
 #EXAMPLE
 
+#EX_1
 print("Hello Nadeem")
 print("I'm from Cochin")
 print('My age is 21')
 
 
 
+#EX_2
 company_name = "Brototype"
 location = "Maradu"
 print(company_name)
 print(location)
 
 
+#EX_3
 name = "Nadeem"
 age = 21
 color = "Black"
@@ -30,4 +33,35 @@ color = "Black"
 #ADDING STMTS TO OUTPUT
 
 print(company_name,"is located at",location)
+print(name,age,"years old like",color,"color")
+
+
+
+#INPUT FUNCTIONS
+
+    #Fuction used to read or recieve data in console
+        #scanf(C)
+        #cin(C++)
+        #input(python)
+
+
+#EXAMPLE
+
+#EX_1
+num1 = input("Enter your first number")
+num2 = input("Enter your second number")
+print("You first number is",num1)
+print("You second number is",num2)
+
+
+#EX_2
+company_name = input("Enter your company name")
+location = input("Enter company location")
+print(company_name,"is located at",location)
+
+
+#EX_3
+name = input("Enter your name")
+age = input("Enter your age")
+color = input("Enter your favorite color")
 print(name,age,"years old like",color,"color")
